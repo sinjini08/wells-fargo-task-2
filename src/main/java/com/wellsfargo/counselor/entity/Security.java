b8a75be5ee982a372e8f99db61ae2a91602a7f81
User <user@Users-MacBook-Pro.local>
2025-12-24 07:03:31 -0500

    Implement data model entities for Task 2

---
+++ src/main/java/com/wellsfargo/counselor/entity/Security.java
@@ -0,0 +1,89 @@
+package com.wellsfargo.counselor.entity;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+
+@Entity
+public class Security {
+
+    @Id
+    @GeneratedValue
+    private long securityId;
+
+    @Column(nullable = false)
+    private long portfolioId;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String category;
+
+    @Column(nullable = false)
+    private double purchasePrice;
+
+    @Column(nullable = false)
+    private int quantity;
+
+  
+    protected Security() {
+    }
+
+    public Security(long portfolioId,
+                    String name,
+                    String category,
+                    double purchasePrice,
+                    int quantity) {
+
+        this.portfolioId = portfolioId;
+        this.name = name;
+        this.category = category;
+        this.purchasePrice = purchasePrice;
+        this.quantity = quantity;
+    }
+
+    public long getSecurityId() {
+        return securityId;
+    }
+
+    public long getPortfolioId() {
+        return portfolioId;
+    }
+
+    public void setPortfolioId(long portfolioId) {
+        this.portfolioId = portfolioId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public double getPurchasePrice() {
+        return purchasePrice;
+    }
+
+    public void setPurchasePrice(double purchasePrice) {
+        this.purchasePrice = purchasePrice;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+}
